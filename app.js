44d7e960c5398983546dbd896f83af389f640823
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-04-24 01:02:59 +0100

    enabled cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const connectDb = require("./db/connect");
 const dotenv = require("dotenv").config();
 const create = require("./routes/create");
 const cors = require("cors");
-// enable cors
+// enabled cors
 app.use(cors());
 // middleWears
 app.use(express.json());
@@ -15,7 +15,9 @@ const port = process.env.PORT || 8000;
 
 const start = async () => {
   try {
-    await connectDb(process.env.MONGO_URI);
+    await connectDb(
+      "mongodb+srv://admin-tayo:1234@cluster0.hsis8td.mongodb.net/Gaming?retryWrites=true&w=majority"
+    );
     app.listen(port, () => {
       console.log(`server is running on port: ${port}`);
     });
